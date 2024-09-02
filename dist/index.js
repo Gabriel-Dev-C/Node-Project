31f89d0efb122a1b91f6fc2fccd662ee5faf127c
Gabriel <gacmartins@gmail.com>
2024-09-01 21:30:20 -0300

    Criando o Projeto Node

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const number = 18;
+if (number >= 20) {
+    console.log('Number are greater than 18');
+}
+else {
+    console.log('Number are less than 18');
+}
+console.log('Hello World', number);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// PARA EXECUTAR O ARQUIVO: 
+// PRIMEIRO: npx tsc
+// SEGUNDO: node dist/index.js
+
+const number : number = 18;
+
+if (number >= 20) {
+    console.log('Number are greater than 18');
+} else {
+    console.log('Number are less than 18');
+}
+
+console.log('Hello World', number);
